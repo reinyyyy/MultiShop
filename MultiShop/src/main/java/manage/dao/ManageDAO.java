47fb04eba48fa1a2c872a8218158b26758ed0c9a
minggul <forrest_gump_@naver.com>
2018-11-24 01:19:12 +0900

    종훈이형 컨트롤러 및 페이지 레이아웃 합침

---
+++ MultiShop/src/main/java/manage/dao/ManageDAO.java
@@ -0,0 +1,5 @@
+package manage.dao;
+
+public interface ManageDAO {
+
+}
---
+++ MultiShop/src/main/java/manage/dao/MangeMybatis.java
@@ -0,0 +1,13 @@
+package manage.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class MangeMybatis implements ManageDAO {
+	@Autowired
+	private SqlSession sqlSession;
+}
