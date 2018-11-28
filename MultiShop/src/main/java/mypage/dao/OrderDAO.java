e82085e11d225b799ac933d420b07e8f9c2d76cb
reiny <reinyyyy@gmail.com>
2018-11-28 16:24:21 +0900

    jekal new branch

---
+++ MultiShop/src/main/java/mypage/dao/OrderDAO.java
@@ -0,0 +1,20 @@
+package mypage.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import mypage.bean.OrderDTO;
+
+@Transactional
+@Component
+public interface OrderDAO {
+	
+	public List<OrderDTO> orderList(Map<String,String> map);
+	
+	public int totalA();
+
+	public List<OrderDTO> orderAllList(Map<String,String> map);
+}
